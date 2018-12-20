f4275cffb36c5a001433ca3b4ca98fe66786a04c
漆黑中的追迹者 <765456149@qq.com>
2018-12-21 00:33:03 +0800

    “回字的的四种写法”
    一个问题，输入m.n参数，获取一个m长度的都是n的数组

--- typescript-test/js/test.js
+++ typescript-test/js/test.js
@@ -21,44 +21,8 @@ function createName(name) {
 }
 var greetingMessage = "Greetings, " + createName(["Sam", "Smith"]);
 alert(greetingMessage);
-// class Animal {
-//     constructor(public  name: string) {
-//     };
-//
-//     say(sound: string = '叫') {
-//         console.log(`${this.name},${sound}`)
-//     }
-// }
-//
-// class Dog extends Animal {
-//     constructor(public name: string) {
-//         super(name);
-//     }
-//
-//     say(sound: string = '汪汪') {
-//         super.say(sound);
-//     }
-//
-//     jump(name: string) {
-//         console.log(`${name}正在一边${this.say()}，一边跳`);
-//     }
-// }
-//
-// class Cat extends Animal {
-//     constructor(public name: string) {
-//         super(name);
-//     }
-//
-//     say(sound: string = '喵喵喵') {
-//         super.say(sound);
-//     }
-// }
-//
-// let ergou = new Dog('二狗');
-// ergou.say('大家好!');
-// ergou.jump('二狗');
-// let ermao = new Cat('二猫');
-// ermao.say();
-//
-// let sangou = new Dog('三狗');
-// sangou.say('三狗汪汪汪');
+(function (m, n) {
+    var a = new Array(m + 1).join(',' + n);
+    var b = a.split(',');
+    return b.slice(1);
+})(10, 'abc');
--- typescript-test/js/test.ts
+++ typescript-test/js/test.ts
@@ -10,62 +10,25 @@ testfunction2('hello')
 type NameOrNameArray = string | string[];
 type strNumber = string | number;
 
-function add(a:strNumber,b:strNumber) {
-    return Number(a)+Number(b);
+function add(a: strNumber, b: strNumber) {
+    return Number(a) + Number(b);
 }
 
-console.log(add('123',546))
+console.log(add('123', 546))
+
 function createName(name: NameOrNameArray) {
     if (typeof name === "string") {
         return name;
-    }
-    else {
+    } else {
         return name.join(" ");
     }
 }
 
-var greetingMessage = `Greetings, ${ createName(["Sam", "Smith"]) }`;
+var greetingMessage = `Greetings, ${createName(["Sam", "Smith"])}`;
 alert(greetingMessage);
 
-// class Animal {
-//     constructor(public  name: string) {
-//     };
-//
-//     say(sound: string = '叫') {
-//         console.log(`${this.name},${sound}`)
-//     }
-// }
-//
-// class Dog extends Animal {
-//     constructor(public name: string) {
-//         super(name);
-//     }
-//
-//     say(sound: string = '汪汪') {
-//         super.say(sound);
-//     }
-//
-//     jump(name: string) {
-//         console.log(`${name}正在一边${this.say()}，一边跳`);
-//     }
-// }
-//
-// class Cat extends Animal {
-//     constructor(public name: string) {
-//         super(name);
-//     }
-//
-//     say(sound: string = '喵喵喵') {
-//         super.say(sound);
-//     }
-// }
-//
-// let ergou = new Dog('二狗');
-// ergou.say('大家好!');
-// ergou.jump('二狗');
-// let ermao = new Cat('二猫');
-// ermao.say();
-//
-// let sangou = new Dog('三狗');
-// sangou.say('三狗汪汪汪');
-
+((m, n) => {
+    let a = new Array(m + 1).join(',' + n)
+    let b = a.split(',');
+    return b.slice(1);
+})(10, 'abc')
